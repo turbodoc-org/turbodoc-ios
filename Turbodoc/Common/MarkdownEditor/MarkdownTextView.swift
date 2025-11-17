4d9d5842e6167aea1a622306d1389c6424f718d4
Nico <nicolaasb@pm.me>
2025-11-17 22:17:10 +0100

    Update MarkdownTextView.swift

--- Turbodoc/Common/MarkdownEditor/MarkdownTextView.swift
+++ Turbodoc/Common/MarkdownEditor/MarkdownTextView.swift
@@ -3,6 +3,9 @@ import UIKit
 class MarkdownTextView: UITextView {
     private let markdownStorage = MarkdownTextStorage()
     
+    // External delegate to forward UITextViewDelegate methods
+    weak var externalDelegate: UITextViewDelegate?
+    
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         // Create the text system components
         let layoutManager = NSLayoutManager()
@@ -34,11 +37,41 @@ class MarkdownTextView: UITextView {
         
         // Important for smooth typing
         layoutManager.allowsNonContiguousLayout = true
+        
+        // Set self as the delegate to intercept calls
+        delegate = self
     }
     
     // Helper to get/set plain markdown text
     var markdownText: String {
         get { return text ?? "" }
         set { text = newValue }
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension MarkdownTextView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        externalDelegate?.textViewDidChange?(textView)
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        externalDelegate?.textViewDidChangeSelection?(textView)
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        return externalDelegate?.textViewShouldBeginEditing?(textView) ?? true
+    }
+    
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        return externalDelegate?.textViewShouldEndEditing?(textView) ?? true
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        externalDelegate?.textViewDidBeginEditing?(textView)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        externalDelegate?.textViewDidEndEditing?(textView)
     }
 }
